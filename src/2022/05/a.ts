1dd5c75b1e35dd81cbdcf0f243fcca092522285a
k4ds3 <5180118+k4ds3@users.noreply.github.com>
2022-12-05 19:48:59 +0100

    feat: add day 5

---
+++ src/2022/05/a.ts
@@ -0,0 +1,30 @@
+import * as fs from "fs";
+
+fs.readFile('in.txt', 'utf-8', (err, data) => {
+    if (err) console.log(err);
+
+    let stacks: string[][] = []
+    const iter = data.split("\n")[Symbol.iterator]();
+    let line: string;
+    while((line = iter.next().value) !== undefined) {
+        if(line.includes("[")) {
+            const cleanedLine = line.replace(/(?<keep>...)( |\n|$)/g, "$<keep>").replace(/\[|\]/g, "").replace(/ {3}/g, "-");
+            console.log(cleanedLine)
+            for (let i = 0; i < cleanedLine.split("").length; i++) {
+                if (cleanedLine[i] !== "-") {
+                    if (!stacks[i]) stacks[i] = [];
+                    stacks[i].push(cleanedLine[i]);
+                }
+            }
+        } else if(line.trim() === "") {
+            console.log(stacks)
+        } else if(line.startsWith("move")) {
+            const [amount, from, to] = line.split(/[a-z]+/g).filter(Boolean).map(value => parseInt(value));
+            for (let i = 0; i < amount; i++) {
+                stacks[to-1].unshift(stacks[from-1].shift()!);
+            }
+        }
+    }
+
+    console.log(stacks.reduce((s, stack) => {return s + stack[0]}, ""));
+});
---
+++ src/2022/05/b.ts
@@ -0,0 +1,28 @@
+import * as fs from "fs";
+
+fs.readFile('in.txt', 'utf-8', (err, data) => {
+    if (err) console.log(err);
+
+    let stacks: string[][] = []
+    const iter = data.split("\n")[Symbol.iterator]();
+    let line: string;
+    while((line = iter.next().value) !== undefined) {
+        if(line.includes("[")) {
+            const cleanedLine = line.replace(/(?<keep>...)( |\n|$)/g, "$<keep>").replace(/\[|\]/g, "").replace(/ {3}/g, "-");
+            console.log(cleanedLine)
+            for (let i = 0; i < cleanedLine.split("").length; i++) {
+                if (cleanedLine[i] !== "-") {
+                    if (!stacks[i]) stacks[i] = [];
+                    stacks[i].push(cleanedLine[i]);
+                }
+            }
+        } else if(line.trim() === "") {
+        } else if(line.startsWith("move")) {
+            const [amount, from, to] = line.split(/[a-z]+/g).filter(Boolean).map(value => parseInt(value));
+            stacks[to-1] = stacks[from-1].slice(0, amount).concat(stacks[to-1]);
+            stacks[from-1] = stacks[from-1].slice(amount);
+        }
+    }
+
+    console.log(stacks.reduce((s, stack) => {return s + stack[0]}, ""));
+});
