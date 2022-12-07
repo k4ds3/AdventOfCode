5da3ddb0d1d7137069df1eb3f567ceaf71c25008
k4ds3 <5180118+k4ds3@users.noreply.github.com>
2022-12-07 13:24:25 +0100

    refactor: significantly reduce code for day 7

--- src/2022/07/b.ts
+++ src/2022/07/b.ts
@@ -4,39 +4,24 @@ fs.readFile(process.argv[2] ?? "in.txt", "utf-8", (err, data) => {
     if (err) console.log(err);
 
     const lines = data.trim().split("\n");
-    const fileTree: {[key: string]: any} = {};
     const directoryMetaData: {[key: string]: number} = {};
     const currentDirectory: string[] = [];
-    let currentDirectoryRef: any = fileTree;
     let i = 0;
+
     while(i < lines.length) {
         if(lines[i].startsWith("$ cd ")) {
             const targetDir = lines[i].substring("$ cd ".length);
             if(targetDir === "..") {
                 currentDirectory.pop();
-                i++;
-                continue;
+            } else {
+                currentDirectory.push(targetDir);
             }
 
-            currentDirectory.push(targetDir);
-            let parentElement = fileTree;
-            currentDirectory.forEach(pathElement => {
-                if(!parentElement[pathElement]) {
-                    parentElement[pathElement] = {};
-                }
-                parentElement = parentElement[pathElement];
-                currentDirectoryRef = parentElement;
-            });
             i++;
         } else if(lines[i].startsWith("$ ls")) {
             i++;
             while(i < lines.length && !lines[i].startsWith("$")) {
-                if(lines[i].startsWith("dir ")) {
-                    if(!currentDirectoryRef[lines[i].split(" ")[1]]) {
-                        currentDirectoryRef[lines[i].split(" ")[1]] = {};
-                    }
-                } else {
-                    const fileName = lines[i].split(" ")[1];
+                if(!lines[i].startsWith("dir ")) {
                     const fileSize = lines[i].split(" ")[0];
                     let path = "";
                     for (const pathPart of currentDirectory) {
@@ -46,8 +31,6 @@ fs.readFile(process.argv[2] ?? "in.txt", "utf-8", (err, data) => {
                         }
                         directoryMetaData[path] += parseInt(fileSize);
                     }
-
-                    currentDirectoryRef[fileName] = fileSize;
                 }
                 i++;
             }
